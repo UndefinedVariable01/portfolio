41acd269ea84cf6b0738d028c2ff164267a41517
uva <uva00110001@gmail.com>
2021-04-22 13:46:41 +0430

    Added email address and dynamic age calculation

--- components/About.js
+++ components/About.js
@@ -6,6 +6,11 @@ const { sectionGapTop, sectionGapBottom, blockImageContainer } = utils
 const { about, title, imageContainer, container, c1, c2, c3, c4, c5, c6, c7, c8, c9, question, answer } = styles
 
 export default function About() {
+    const birthDate = new Date("1999-04-25")
+    const now = new Date()
+    const ageDate = new Date(now.getTime() - birthDate.getTime())
+    const ageYears = ageDate.getUTCFullYear() - 1970
+
     return (
         <div className={`${sectionGapTop} ${sectionGapBottom} ${about}`} id="about">
             <h1 className={title}>Wanna Know More About Me?</h1>
@@ -22,7 +27,7 @@ export default function About() {
             </div>
             <div className={`${container} ${c3}`}>
                 <p className={question}>How old are you?</p>
-                <p className={answer}>21 years old</p>
+                <p className={answer}>{ageYears} years old</p>
             </div>
             <div className={`${container} ${c4}`}>
                 <p className={question}>Are your a front-end or back-end developer?</p>
@@ -30,7 +35,9 @@ export default function About() {
             </div>
             <div className={`${container} ${c5}`}>
                 <p className={question}>How long have your been coding for?</p>
-                <p className={answer}>3 years(1.5 years web dev)</p>
+                <p className={answer}>
+                    {ageYears - 18} years(${ageYears - 20}.5 years web dev)
+                </p>
             </div>
             <div className={`${container} ${c6}`}>
                 <p className={question}>What technologies can you work with?</p>
@@ -49,7 +56,7 @@ export default function About() {
             </div>
             <div className={`${container} ${c9}`}>
                 <p className={question}>How can i contact you?</p>
-                <p className={answer}>You can use my email: uva@gmail.com or the contact form below</p>
+                <p className={answer}>You can use my email: uva00110001@gmail.com or the contact form below</p>
             </div>
         </div>
     )
