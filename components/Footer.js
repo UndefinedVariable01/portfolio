43b2bed203f6d32f7d7b3434f68cd2a9081ce020
Uva <uva00110001@gmail.com>
2022-02-19 12:30:43 +0330

    changed footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ const { footer, text, icon } = styles
 export default function Footer() {
     return (
         <footer className={`${footer}`}>
-            <p className={text}>Designed And Developed By Uva</p>
-            <a href="https://github.com/UndefinedVariable01">
+            <p className={text}>Designed And Developed By m0hammadr3za</p>
+            <a href="https://github.com/m0hammadr3za">
                 <img className={icon} src="/github.svg" />
             </a>
         </footer>
